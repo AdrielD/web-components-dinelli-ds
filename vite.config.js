3f55da6fdd51a92cc09bf53a8ee8034f531777dd
Adriel Dinelli <adriel.dinelli@gmail.com>
2023-12-27 08:17:28 -0300

    Fiddling with configs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [
+    minifyHTML.default(),
+  ],
   test: {
     include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)', '**/test.ts'],
     environment: 'happy-dom',
@@ -12,11 +16,14 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'dinelli-ds',
       fileName: 'dinelli-ds',
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: [],
+      external: ['lit'],
       output: {
-        globals: {},
+        globals: {
+          lit: 'lit'
+        },
       },
     },
   },
